df3e366d3ae954d8044b00dc905fa661d4c4d2d8
eicheich <nengindah41@gmail.com>
2024-09-29 17:28:53 +0700

    [HK] setup migration

---
+++ app/Http/Controllers/V1/Web/PostsReferenceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostsReference;
+use Illuminate\Http\Request;
+
+class PostsReferenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PostsReference $postsReference)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PostsReference $postsReference)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PostsReference $postsReference)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PostsReference $postsReference)
+    {
+        //
+    }
+}
